e5fd6995bb8abdde6a3c0a991be79d5e8091f7eb
Nabil001 <nabil.lemenuel@gmx.fr>
2018-02-27 23:09:31 +0100

    created a service in charge of refreshing user roles if needed

---
+++ src/AppBundle/Utils/UserRefresher.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Utils;
+
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class UserRefresher
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @param User $user
+     * @return void
+     */
+    public function refreshRolesIfAuthenticated(User $user)
+    {
+        $authenticatedUser = $this->tokenStorage->getToken()->getUser();
+
+        if (is_a($authenticatedUser, User::class) && $authenticatedUser->getUsername() == $user->getUsername()) {
+            $this->tokenStorage->setToken(
+                new UsernamePasswordToken(
+                    $user,
+                    null,
+                    'main',
+                    $user->getRoles()
+                )
+            );
+        }
+    }
+}
