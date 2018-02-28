dccdffecaf9091fad00db5e076455f0e452609e1
Nabil001 <nabil.lemenuel@gmx.fr>
2018-02-28 12:02:01 +0100

    a service is in charge of checking if current user can delete a given task

---
+++ src/AppBundle/Utils/TaskAuthorizationChecker.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Utils;
+
+use AppBundle\Entity\Task;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class TaskAuthorizationChecker
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * @param Task $task
+     * @return bool
+     */
+    public function isAllowedToDelete(Task $task)
+    {
+        $author = $task->getAuthor();
+        $user = $this->tokenStorage->getToken()->getUser();
+        $isAdmin = $this->authorizationChecker->isGranted('ROLE_ADMIN');
+
+        $isAuthenticated = $this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY');
+        if (null === $author && !$isAdmin || null !== $author && (!$isAuthenticated || $user->getUsername() != $author->getUsername())) {
+            return false;
+        }
+        return true;
+    }
+}
